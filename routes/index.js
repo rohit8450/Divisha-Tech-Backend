67e55fa82bde0f69a95ca3b68c21373631b50787
rohit8450 <rohitrajthakur10@gmail.com>
2023-06-09 19:24:00 +0530

    backend

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+
+    return res.json({ "Welcome":  `Welcome to the Divisha Techmology Backend ` });
+
+});
+
 router.use('/api/seller', require('./register'));
 router.use('/api/seller', require('./auth'));
 router.use('/api/seller', require('./store'));
